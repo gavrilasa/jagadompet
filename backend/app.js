b93d7663e431101f8ec0e1a2484546c143354236
Rhe011 <rhe.aoxx@gmail.com>
2025-05-04 09:42:39 +0700

    skema database

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/auth.routes.js";
+import transactionRoutes from "./routes/transactions.js";
+import dashboardRoutes from "./routes/dashboard.js";
 
 dotenv.config();
 
@@ -14,5 +16,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/transactions", transactionRoutes);
+app.use("/api/dashboard", dashboardRoutes);
 
 export default app;
--- backend/models/transaction.model.js
+++ backend/models/transaction.model.js
@@ -1,24 +1,42 @@
 import { Schema, model } from "mongoose";
+import { v4 as uuidv4 } from "uuid";
 
-const transactionSchema = new Schema({
-  type: {
-    type: String,
-    enum: ["income", "expense"],
-    required: true,
+const transactionSchema = new Schema(
+  {
+    transaction_id: {
+      type: String,
+      default: uuidv4,
+      unique: true,
+    },
+    user_id: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ["income", "expense"],
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    }
   },
-  amount: {
-    type: Number,
-    required: true,
-  },
-  category: {
-    type: String,
-    required: true,
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
-  note: String
-});
+  {
+    timestamps: { createdAt: "created_at", updatedAt: "updated_at" },
+  }
+);
 
 export default model("Transaction", transactionSchema);
